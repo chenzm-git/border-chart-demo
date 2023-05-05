a2f4fee58d9f21ab0fd1d15db973fccd850b13bd
chenzm-git <chenzm5@asiainfo-sec.com>
2023-05-05 09:22:07 +0800

    增加chart接口

---
+++ src/main/java/com/tencent/wxcloudrun/controller/ChartController.java
@@ -0,0 +1,29 @@
+package com.tencent.wxcloudrun.controller;
+
+import com.tencent.wxcloudrun.service.impl.ChartServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author chenzm
+ * @version V1.0
+ * @since 2023-05-05$
+ **/
+@RestController
+@Slf4j
+public class ChartController {
+
+    @Autowired
+    private ChartServiceImpl chartService;
+
+    @PostMapping(value = "/api/chart")
+    public String chart(@RequestBody String input){
+        log.info("chart 输入 : " + input);
+        String output = chartService.chart(input);
+        log.info("chart 输出 : " + output);
+        return output;
+    }
+}
---
+++ src/main/java/com/tencent/wxcloudrun/service/impl/ChartServiceImpl.java
@@ -0,0 +1,63 @@
+package com.tencent.wxcloudrun.service.impl;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author chenzm
+ * @version V1.0
+ * @since 2023-05-05$
+ **/
+@Service
+public class ChartServiceImpl {
+
+    private CloseableHttpClient httpclient = null;
+
+    private String url = "http://23.247.131.178:8080/api/chart";
+
+    public String chart(String input){
+        String ret = "";
+
+        if (httpclient == null) {
+            httpclient = HttpClients.createDefault();
+        }
+
+        try {
+            HttpPost post = new HttpPost(url);
+
+            StringEntity entityIn = new StringEntity(new JSONObject(input).toString());
+            post.setEntity(entityIn);
+
+            // 执行get请求
+            CloseableHttpResponse response = httpclient.execute(post);
+
+            try {
+                // 获取响应实体
+                HttpEntity entity = response.getEntity();
+                if (entity != null) {
+                    // 返回响应内容
+                    ret = EntityUtils.toString(entity);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                response.close();
+            }
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return ret;
+    }
+
+
+}
